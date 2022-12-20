9e524ae44cc00b7b290229fb85b5adfeeceeb8ba
Adnan <adnans0307@gmail.com>
2022-12-20 20:21:52 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {SidebarModule} from 'primeng/sidebar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
     DashboardComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
