204097d25d7b199feda516b19f66332d791abbb1
Rohan Walke <rohanwalke2002@gmail.com>
2023-01-18 20:44:58 +0530

    ok

--- src/app/user/pages/attendance/attendance.component.ts
+++ src/app/user/pages/attendance/attendance.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { SelectItem } from 'primeng/api';
+import { SelectItemGroup } from 'primeng/api';
+
+interface City {
+  name: string,
+  code: string
+}
 
 @Component({
   selector: 'app-attendance',
@@ -6,5 +14,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./attendance.component.scss']
 })
 export class AttendanceComponent {
+   
+  cities: City[];
+  selectedCity1: City | undefined;
+  selectedCity2: City | undefined;
 
+  items: SelectItem[];
+  Years: { name: string; code: string; }[];
+
+
+
+
+  constructor() {
+    this.items = [];
+    for (let i = 0; i < 10000; i++) {
+      this.items.push({ label: 'Item ' + i, value: 'Item ' + i });
+    }
+
+    this.cities = [
+      { name: 'January', code: 'Jan' },
+      { name: 'February', code: 'Feb' },
+      { name: 'March', code: 'Mar' },
+      { name: 'April', code: 'Apr' },
+      { name: 'May', code: 'May' },
+      { name: 'June', code: 'Jun' },
+      { name: 'July', code: 'Jul' },
+      { name: 'August', code: 'Aug' },
+      { name: 'September', code: 'Sep' },
+      { name: 'October', code: 'Oct' },
+      { name: 'November', code: 'Nov' },
+      { name: 'December', code: 'Dec' },
+    ];
+    
+       this.Years = [
+      { name: '2023', code: '23' },
+      { name: '2024', code: '24' },
+      { name: '2025', code: '25' },
+      { name: '2026', code: '26' },
+      { name: '2027', code: '27' },
+      { name: '2028', code: '28' },
+      { name: '2029', code: '29' },
+
+    ];
+
+
+  }
 }
