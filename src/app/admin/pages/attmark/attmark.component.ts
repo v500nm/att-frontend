3e6108c62a91306236c03ba4167eb8267ac338a3
Adnan <adnans0307@gmail.com>
2023-02-27 22:01:15 +0530

    ok

--- src/app/admin/pages/attmark/attmark.component.ts
+++ src/app/admin/pages/attmark/attmark.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../service/admin.service';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Iattendance, Igroups, Ischedule } from '../../service/admin.interface';
+import {
+  Iattendance,
+  Iclassroom,
+  Ifaculties,
+  Igroups,
+  Ischedule,
+  Istudents,
+  Isubject,
+} from '../../service/admin.interface';
 
-interface Country {
-  name: string;
-  code: string;
-}
 @Component({
   selector: 'app-attmark',
   templateUrl: './attmark.component.html',
@@ -22,23 +26,58 @@ export class AttmarkComponent implements OnInit {
   ) {}
   display: boolean = false;
   loading: boolean = true;
+  stuSwitch: boolean = true;
 
   showDialog() {
     this.display = true;
   }
+  stuValue!: FormGroup;
+  facValue!: FormGroup;
+  subValue!: FormGroup;
+  classValue!: FormGroup;
   schValue!: FormGroup;
   grpValue!: FormGroup;
   attValue!: FormGroup;
 
+  attData: Iattendance[] = [];
+  stuData: Istudents[] = [];
+  facData: Ifaculties[] = [];
+  subData: Isubject[] = [];
+  classData: Iclassroom[] = [];
   schData: Ischedule[] = [];
   grpData: Igroups[] = [];
-  attData: Iattendance[] = [];
+
 
   ngOnInit() {
+    //forms
+    this.findAllStudents();
+    this.getAllFaculties();
+    this.findAllSubjects();
+    this.findAllClass();
     this.findAllSchedule();
-    this.findAllGroups();
+    this.findAllGroup();
     this.findAllAtt();
 
+    (this.stuValue = this.formsbuilder.group({
+      roll: new FormControl(''),
+      name: new FormControl(''),
+      classGrp: new FormControl(''),
+    })),
+      (this.facValue = this.formsbuilder.group({
+        fname: new FormControl(''),
+        department: new FormControl(''),
+        designation: new FormControl(''),
+      })),
+      (this.subValue = this.formsbuilder.group({
+        subject: new FormControl(''),
+      })),
+      (this.classValue = this.formsbuilder.group({
+        class: new FormControl(''),
+      })),
+      (this.grpValue = this.formsbuilder.group({
+        gName: new FormControl(''),
+        students: new FormControl(''),
+      }));
     this.schValue = this.formsbuilder.group({
       scheduleName: new FormControl(''),
       Date: new FormControl(''),
@@ -49,34 +88,241 @@ export class AttmarkComponent implements OnInit {
       subjects: new FormControl(''),
       classrooms: new FormControl(''),
     });
-    this.grpValue = this.formsbuilder.group({
-      gName: new FormControl(''),
-      students: new FormControl(''),
+
+  
+    this.adminService.findAllSchedule().subscribe((res) => {
+      this.schData = res;
+      this.loading = false;
     });
+ 
   }
 
   //att
-  findAllAtt(){
-    this.adminService.findAllAtt().subscribe((res:Iattendance[])=>{
-      this.attData=res;
-      console.log(res, 'att get')
-    })
+  findAllAtt() {
+    this.adminService.findAllAtt().subscribe((res: Iattendance[]) => {
+      this.attData = res;
+      console.log(res, 'att get');
+    });
   }
-  markAtt(){
-    this.attData=this.attValue.value;
+  markAtt() {
+    this.attData = this.attValue.value;
     console.log(this.attData);
-    this.adminService.postAtt(this.attValue.value)
-    .subscribe((res)=>{
-      console.log(res,'att post');
+    this.adminService.postAtt(this.attValue.value).subscribe((res) => {
+      console.log(res, 'att post');
     });
     this.findAllAtt();
   }
 
-  //Schedule
+  //groups
+  findAllGroup() {
+    this.adminService.findAllGroup().subscribe((res: Igroups[]) => {
+      this.grpData = res;
+      console.log(res, 'groups get');
+    });
+  }
+  createGroup() {
+    this.grpData = this.grpValue.value;
+    this.adminService.createGroup(this.grpValue.value).subscribe((res) => {
+      console.log(res, 'group Post');
+      this.grpValue.reset();
+    });
+  }
+  removeGrp(_id: string) {
+    this.adminService.removeGroup(_id).subscribe((res) => {
+      console.log(res, 'delete works');
+      this.findAllGroup();
+    });
+  }
+  recoverGrp(grpData: any) {
+    delete grpData.__v;
+    this.grpValue.addControl('_id', new FormControl(''));
+    this.grpValue.setValue(grpData);
+    this.stuSwitch = false;
+    this.showDialog();
+    console.log(this.grpData, 'grp coming');
+  }
+  updateGrp() {
+    const id = this.grpValue.value._id;
+    this.adminService
+      .updateGroup(id, this.grpValue.value)
+      .subscribe((res: any) => {
+        this.grpData = res;
+        this.stuSwitch = true;
+        this.findAllGroup();
+      });
+    this.grpValue.reset();
+  }
+
+  //students
+  findAllStudents() {
+    this.adminService.findAllStudents().subscribe((res: Istudents[]) => {
+      this.stuData = res;
+      console.log(res, 'students get');
+    });
+  }
+  regStudents() {
+    this.stuData = this.stuValue.value;
+    this.adminService.registerStudent(this.stuValue.value).subscribe((res) => {
+      console.log(res, 'students post');
+      this.stuValue.reset();
+      this.findAllStudents();
+    });
+  }
+
+  removeStudent(_id: string) {
+    this.adminService.removeStudent(_id).subscribe((res) => {
+      console.log(res, 'delete works');
+      this.findAllStudents();
+    });
+  }
+  recoverStudent(stuData: any) {
+    delete stuData.__v;
+    this.stuValue.addControl('_id', new FormControl(''));
+    this.stuValue.setValue(stuData);
+    this.stuSwitch = false;
+    this.showDialog();
+    console.log(this.stuData, 'stu coming');
+  }
+  updateStudents() {
+    const id = this.stuValue.value._id;
+    this.adminService
+      .updateStudent(id, this.stuValue.value)
+      .subscribe((res: any) => {
+        this.stuData = res;
+        this.stuSwitch = true;
+        this.findAllStudents();
+      });
+    this.stuValue.reset();
+  }
+
+  //faculties
+  getAllFaculties() {
+    this.adminService.getAllFaculties().subscribe((res: Ifaculties[]) => {
+      this.facData = res;
+      console.log(res, 'faculty get');
+    });
+  }
+  postFac() {
+    this.facData = this.facValue.value;
+    console.log(this.facData);
+    this.adminService.addFaculty(this.facValue.value).subscribe((res) => {
+      console.log(res, 'faculty post');
+      this.facValue.reset();
+      this.getAllFaculties();
+    });
+  }
+  removeFaculty(_id: string) {
+    this.adminService.removeFaculty(_id).subscribe((res) => {
+      console.log(res, 'delete works');
+      this.getAllFaculties();
+    });
+  }
+  recoverFaculty(facData: any) {
+    delete facData.__v;
+    this.facValue.addControl('_id', new FormControl(''));
+    this.facValue.setValue(facData);
+    this.stuSwitch = false;
+    this.showDialog();
+    console.log(this.facData, 'fac coming');
+  }
+  updateFac() {
+    const id = this.facValue.value._id;
+    this.adminService
+      .updateFaculty(id, this.facValue.value)
+      .subscribe((res: any) => {
+        this.facData = res;
+        this.stuSwitch = true;
+        this.getAllFaculties();
+      });
+    this.facValue.reset();
+  }
+
+  //subjects
+  findAllSubjects() {
+    this.adminService.findAllSubjects().subscribe((res: Isubject[]) => {
+      this.subData = res;
+      console.log(res, 'subject get');
+    });
+  }
+  postSub() {
+    this.subData = this.subValue.value;
+    console.log(this.subData);
+    this.adminService.addSubject(this.subValue.value).subscribe((res) => {
+      console.log(res, 'subject post');
+      this.subValue.reset();
+      this.findAllSubjects();
+    });
+  }
+  removeSubject(_id: string) {
+    this.adminService.removeSubject(_id).subscribe((res) => {
+      this.findAllSubjects();
+      console.log(res, 'delete works');
+    });
+  }
+  recoverSubject(subData: any) {
+    delete subData.__v;
+    this.subValue.addControl('_id', new FormControl(''));
+    this.subValue.setValue(subData);
+    this.stuSwitch = false;
+    this.showDialog();
+    console.log(this.subData, 'sub coming');
+  }
+  updateSubject() {
+    const id = this.subValue.value._id;
+    this.adminService
+      .updateSubject(id, this.subValue.value)
+      .subscribe((res: any) => {
+        this.subData = res;
+        this.stuSwitch = true;
+        this.findAllSubjects();
+      });
+    this.subValue.reset();
+  }
+  //classroom
+  findAllClass() {
+    this.adminService.findAllClass().subscribe((res: Iclassroom[]) => {
+      this.classData = res;
+      console.log(res, 'classroom get');
+    });
+  }
+  postClass() {
+    this.classData = this.classValue.value;
+    console.log(this.classData);
+    this.adminService.addClass(this.classValue.value).subscribe((res) => {
+      console.log(res, 'class post');
+      this.classValue.reset();
+      this.findAllClass();
+    });
+  }
+  deleteClass(_id: string) {
+    this.adminService.removeClass(_id).subscribe((res) => {
+      this.findAllClass();
+    });
+  }
+  recoverClass(classData: any) {
+    delete classData.__v;
+    this.classValue.addControl('_id', new FormControl(''));
+    this.classValue.setValue(classData);
+    this.stuSwitch = false;
+    this.showDialog();
+    console.log(this.classData, 'class coming');
+  }
+  updateClass() {
+    const id = this.classValue.value._id;
+    this.adminService
+      .updateClass(id, this.classValue.value)
+      .subscribe((res: any) => {
+        this.classData = res;
+        this.stuSwitch = true;
+        this.findAllClass();
+      });
+    this.classValue.reset();
+  }
+  //schedule
   findAllSchedule() {
     this.adminService.findAllSchedule().subscribe((Response: Ischedule[]) => {
       this.schData = Response;
-      console.log(Response, 'schedule get');
+      console.log(Response, 'allData');
     });
   }
   postSch() {
@@ -85,34 +331,33 @@ export class AttmarkComponent implements OnInit {
     this.adminService
       .createSchedule(this.schValue.value)
       .subscribe((Response) => {
-        console.log(Response, 'schedule post');
+        console.log(Response, 'post Method');
       });
     this.findAllSchedule();
+    this.schValue.reset();
   }
   removeSch(_id: string) {
     this.adminService.removeSchedule(_id).subscribe((res) => {
       this.findAllSchedule();
     });
   }
-
-  //groups
-  findAllGroups() {
-    this.adminService.findAllGroup().subscribe((res: Igroups[]) => {
-      this.grpData = res;
-      console.log(res, 'groups get');
-    });
+  recoverSch(schData: any) {
+    delete schData.__v;
+    this.schValue.addControl('_id', new FormControl(''));
+    this.schValue.setValue(schData);
+    this.stuSwitch = false;
+    this.showDialog();
+    console.log(this.schData, 'sch coming');
   }
-  createGroup() {
-    this.grpData = this.grpValue.value;
-    console.log(this.grpData);
-    this.adminService.createGroup(this.grpValue.value).subscribe((res) => {
-      console.log(res, 'group post');
-    });
-    this.findAllGroups();
-  }
-  removeGroup(_id: string) {
-    this.adminService.removeGroup(_id).subscribe((res) => {
-      this.findAllGroups();
-    });
+  updateSch() {
+    const id = this.schValue.value._id;
+    this.adminService
+      .updateSchedule(id, this.schValue.value)
+      .subscribe((res: any) => {
+        this.schData = res;
+        this.stuSwitch = true;
+        this.findAllSchedule();
+      });
+    this.schValue.reset();
   }
 }
