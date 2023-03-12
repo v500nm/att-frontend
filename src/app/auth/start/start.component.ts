bfa1a26be69bb4476755c0895a0d745fbaf00aa9
Adnan <adnans0307@gmail.com>
2023-03-12 17:27:24 +0530

    ok

--- src/app/auth/start/start.component.ts
+++ src/app/auth/start/start.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/shared/auth.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-start',
